c7869db74f7aacd26e0073d3056e2c6e825fbce0
costar011 <dnaer456@gmail.com>
2021-07-24 14:22:36 +0900

    ssss

--- app.py
+++ app.py
@@ -5,8 +5,14 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def base():
-    return render_template("base.html")
+@app.route("/header")
+def hedaer():
+    return render_template("hedaer.html")
+
+
+@app.route("/footer")
+def footer():
+    return render_template("footer.html")
 
 
 app.run(debug=True)
