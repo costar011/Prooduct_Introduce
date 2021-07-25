1d69328278e69f5217472e60793d9db34d2efd69
costar011 <dnaer456@gmail.com>
2021-07-25 16:51:59 +0900

    css

--- app.py
+++ app.py
@@ -1,23 +1,12 @@
 from flask import Flask, render_template
 
-
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return render_template("base.html")
+    return render_template("home.html")
 
 
-@app.route("/mu1")
-def mu1():
-    return render_template("mu1.html")
-
-
-@app.route("/mu2")
-def mu2():
-    return render_template("mu2.html")
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
